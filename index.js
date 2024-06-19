3b6d38c3d1c63d62a8985758f8b370471a25b449
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-06-19 16:14:14 +0800

    0619

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const app = express();
 app.use(express.static("public"));
 app.use("/", serveIndex("public", { icons: true }));
 
+app.get("/try-sse", (req, res) => {
+  let id = 0;
+
+  res.writeHead(200, {
+    "Context-Type": "text/event/stream; Charset:UTF-8",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+  setInterval(function () {
+    let now = new Date();
+    res.write(`id:${id++}\n`);
+    res.write(`data:${now.toLocaleDateString()}\n\n`);
+  }, 2000);
+});
+
 app.listen(web_port, () => {
   console.log(`伺服器啟動於通訊埠：${web_port}`);
 });
